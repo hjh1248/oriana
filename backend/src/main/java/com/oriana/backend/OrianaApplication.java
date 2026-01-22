ce57cf64d146f3a573d9f779d96724e32306a7f6
하정호 <hjh4212@naver.com>
2026-01-22 17:07:10 +0900

    initial commit

---
+++ backend/src/main/java/com/oriana/backend/OrianaApplication.java
@@ -0,0 +1,13 @@
+package com.oriana.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrianaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/oriana/backend/OrianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriana.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
