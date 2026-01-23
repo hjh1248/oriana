524cb24af7b351550a23873e44d548ba2232b0dc
하정호 <hjh4212@naver.com>
2026-01-24 04:36:44 +0900

    chore: CORS

--- backend/src/main/java/com/oriana/backend/controller/UserController.java
+++ backend/src/main/java/com/oriana/backend/controller/UserController.java
@@ -4,14 +4,12 @@ import com.oriana.backend.dto.UserResponseDto;
 import com.oriana.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@CrossOrigin(originPatterns = "*", allowCredentials = "true")
 public class UserController {
 
     private final UserService userService;
