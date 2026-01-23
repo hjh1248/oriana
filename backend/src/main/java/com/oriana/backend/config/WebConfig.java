b30e3213ab7b6f0aa0e5976fae294bea55efdbd8
하정호 <hjh4212@naver.com>
2026-01-24 04:32:51 +0900

    chore: CORS

--- backend/src/main/java/com/oriana/backend/config/WebConfig.java
+++ backend/src/main/java/com/oriana/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Vue(5173)나 React(3000) 허용
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://52.79.210.0") // Vue(5173)나 React(3000) 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키/인증 정보 포함 허용
